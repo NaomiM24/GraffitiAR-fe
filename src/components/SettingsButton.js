f767c87b7f0ead0f0867a4a47b3247035cc6def6
Harry Keyes <harrykeyes@Harrys-MacBook-Pro-2.local>
2019-12-06 10:06:53 +0000

    format settings files

--- src/components/SettingsButton.js
+++ src/components/SettingsButton.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
-import "./Settings.css"
- 
+import "./Settings.css";
+
 class SettingsButton extends Component {
   render() {
     return (
-      <button id="settingsButton"
-        onMouseDown={this.props.handleClick}><img src='./settings.png' alt='settings'/></button>
+      <button id="settingsButton" onMouseDown={this.props.handleClick}>
+        <img src="./settings.png" alt="settings" />
+      </button>
     );
   }
 }
- 
+
 export default SettingsButton;
