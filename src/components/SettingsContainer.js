7a4472633e28734a174a6b82ee6cfb29763ec14a
Harry Keyes <harrykeyes@Harrys-MacBook-Pro-2.local>
2019-12-06 10:06:53 +0000

    format settings files

--- src/components/SettingsContainer.js
+++ src/components/SettingsContainer.js
@@ -1,30 +1,30 @@
 import React, { Component } from "react";
-import Settings from "./Settings"
-import SettingsButton from "./SettingsButton"
+import Settings from "./Settings";
+import SettingsButton from "./SettingsButton";
 
 class SettingsContainer extends Component {
-  
   state = {
-     visible: false
-  }
-  
+    visible: false,
+  };
 
-  handleClick = (e) => {
-      this.toggleSettings();
-  }
+  handleClick = e => {
+    this.toggleSettings();
+  };
 
   toggleSettings = () => {
     this.setState({
-        visible: !this.state.visible
+      visible: !this.state.visible,
     });
-  }
+  };
 
-  
   render() {
     return (
       <>
-        <SettingsButton handleClick={this.handleClick}/>
-        <Settings handleClick={this.handleClick} settingsVisibility={this.state.visible}/>
+        <SettingsButton handleClick={this.handleClick} />
+        <Settings
+          handleClick={this.handleClick}
+          settingsVisibility={this.state.visible}
+        />
       </>
     );
   }
