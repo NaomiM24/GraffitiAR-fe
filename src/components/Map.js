f5ff0144e0e8dbcbed8610b34e7050c50b16a6b5
Northcoders <northcoders@Northcoderss-MacBook-Pro.local>
2019-12-07 18:05:39 +0000

    intergrated canvas-viewer with geolocation to only view graffiti within 50m

--- src/components/Map.js
+++ src/components/Map.js
@@ -22,7 +22,7 @@ class GMap extends Component {
   componentDidMount() {
     this.getGeoLocation();
     this.setState({ isMarkerShown: true });
-    this.interval = setInterval(() => this.getGeoLocation(), 1000);
+    // this.interval = setInterval(() => this.getGeoLocation(), 1000);
   }
 
   getGeoLocation = () => {
@@ -116,3 +116,15 @@ export default function Map() {
     </div>
   );
 }
+// var R = 6371e3; // metres
+// var φ1 = lat1.toRadians();
+// var φ2 = lat2.toRadians();
+// var Δφ = (lat2-lat1).toRadians();
+// var Δλ = (lon2-lon1).toRadians();
+
+// var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+//         Math.cos(φ1) * Math.cos(φ2) *
+//         Math.sin(Δλ/2) * Math.sin(Δλ/2);
+// var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+// var d = R * c;
