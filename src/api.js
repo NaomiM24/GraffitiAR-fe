19827e947799710289cebec60e66c7f054596e98
Northcoders <northcoders@Northcoderss-MacBook-Pro.local>
2019-12-09 13:19:45 +0000

    post a graffiti with post/err message

--- src/api.js
+++ src/api.js
@@ -1,3 +1,13 @@
 import axios from "axios";
 
 const baseURL = "https://be-geograffiti.herokuapp.com/api";
+
+export const postCanvas = (firebase_id, drawing_str, geo_lat, geo_long) => {
+  return axios.post(`${baseURL}/graffiti`, {
+    firebase_id,
+    drawing_str,
+    geo_lat,
+    geo_long,
+    votes: 0,
+  });
+};
