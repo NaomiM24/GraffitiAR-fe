444ee6e517047892e8940198c8292ac37e92d032
AJINK13 <53950868+AJINK13@users.noreply.github.com>
2019-12-09 14:22:14 +0000

    send firebase user id to our database (#16)
    Slight conflict in the api.js file, so can't rebase and merge. Squash and merged instead.

--- src/api.js
+++ src/api.js
@@ -2,6 +2,10 @@ import axios from "axios";
 
 const baseURL = "https://be-geograffiti.herokuapp.com/api";
 
+export const postUser = userObject => {
+  return axios.post(`${baseURL}/users`, userObject);
+};
+
 export const postCanvas = (firebase_id, drawing_str, geo_lat, geo_long) => {
   return axios.post(`${baseURL}/graffiti`, {
     firebase_id,
@@ -10,4 +14,5 @@ export const postCanvas = (firebase_id, drawing_str, geo_lat, geo_long) => {
     geo_long,
     votes: 0,
   });
+
 };
