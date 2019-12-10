e7e635495124424758e1cc876cf54a496faecd2b
Ajinkya <ajinkya1394@outlook.com>
2019-12-10 09:49:18 +0000

    add routes to api.js

--- src/api.js
+++ src/api.js
@@ -19,3 +19,16 @@ export const postCanvas = (firebase_id, drawing_str, geo_lat, geo_long) => {
 export const getAllGraffiti = () => {
   return axios.get(`${baseURL}/graffiti`);
 };
+
+export const putUsernameByFirebaseID = (newUsername, firebase_id) => {
+  return axios.put(`${baseURL}/users/change_name/${firebase_id}`, {
+    username: newUsername,
+  });
+};
+
+export const putDisplayPicByFirebaseID = (newDisplayPic, firebase_id) => {
+  return axios.put(`${baseURL}/users/change_pic/${firebase_id}`, {
+    newDisplayPic,
+    firebase_id,
+  });
+};
