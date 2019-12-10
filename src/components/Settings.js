9f04bbca34a0f84f1fef3586ee08231a243350c8
Ajinkya <ajinkya1394@outlook.com>
2019-12-10 10:16:48 +0000

    add changeDisplayPic and changeUsername methods to Settings

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,18 +1,33 @@
 import React, { Component } from "react";
 import "./Settings.css";
 import fire from "../config/Fire";
+import * as api from "../api.js"
 
 class Settings extends Component {
   constructor(props) {
     super(props);
     this.logout = this.logout.bind(this);
+    this.changeUsernameByFirebaseID = this.changeUsernameByFirebaseID.bind(this)  
   }
 
   logout() {
     fire.auth().signOut();
   }
 
+  changeUsernameByFirebaseID = () => {
+    const { firebase_id } = this.props.uid;
+    console.log("changeUsername", firebase_id);
+    api.putUsernameByFirebaseID({username: newUsername}, firebase_id)
+  };
+
+  changeDisplayPicByFirebaseID = () => {
+    const { firebase_id } = this.props.uid
+    console.log("changeDisplayPic", firebase_id)
+    api.putDisplayPicByFirebaseID({display_pic_url, newDisplayPic}, firebase_id)
+  }
+
   render() {
+    console.log("settings", this.props.uid);
     var visibility = "hide";
     if (this.props.settingsVisibility) {
       visibility = "show";
@@ -25,7 +40,9 @@ class Settings extends Component {
         </button>
         <h1>Settings</h1>
         <button onClick={this.logout}>Logout</button>
-        <p>Change Username</p>
+        <button onClick={this.changeUsernameByFirebaseID}>Change username</button>
+        {/* <p>Change Username</p> */}
+        <button onClick={this.changeDisplayPicByFirebaseID}>Change display picture</button>
         <p>View My Graffiti</p>
       </div>
     );
