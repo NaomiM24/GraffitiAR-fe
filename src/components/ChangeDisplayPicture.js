e62b350672888335d3af19f420aa7937d182f9d7
Ajinkya <ajinkya1394@outlook.com>
2019-12-10 11:33:36 +0000

    add form to Change Display Picture

--- src/components/ChangeDisplayPicture.js
+++ src/components/ChangeDisplayPicture.js
@@ -4,12 +4,43 @@ import * as api from "../api.js";
 class ChangeDisplayPicture extends Component {
   constructor(props) {
     super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.changeDisplayPictureByFirebaseID = this.changeDisplayPictureByFirebaseID.bind(
+      this
+    );
+    this.state = {
+      newDisplayPicture: "",
+    };
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  changeDisplayPictureByFirebaseID() {
+    api.putDisplayPicByFirebaseID();
   }
 
   render() {
     console.log("ChangeDisplayPicture user", this.props.user);
     console.log("ChangeDisplayPicture uid", this.props.uid);
-    return <div>INSIDE ChangeDisplayPicture</div>;
+    return (
+      <div>
+        <form>
+          <label>
+            New Display Picture
+            <input
+              value={this.state.newDisplayPicture}
+              onChange={this.handleChange}
+              type="image"
+              name="display picture"
+              required
+            />
+          </label>
+          <button onClick={this.changeUsernameByFirebaseID}>Confirm</button>
+        </form>
+      </div>
+    );
   }
 }
 
